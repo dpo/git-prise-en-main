abb44e37c5e6c480f0bb12e36e6d61dff9ff330f
Charles Ménard <charles.menard.1@protonmail.ch>
2024-06-01 10:52:28 -0400

    Ajouter info en header# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch main# Your branch is up to date with 'origin/main'.## Changes to be committed:#	modified:   exercice.jl#

--- exercice.jl
+++ exercice.jl
@@ -1,15 +1,17 @@
+# LABO 1
+# Author : Charles Ménard
+# Date : 2024-06-01
+
 using LinearAlgebra
 
+n = 3
 A = [1.0 2.0 3.0;
     2.0 -5.0 3.0
     3.0 3.5 -4.0]
-b = ones(3)
+b = ones(n)
 
-# 1) résoudre Ax = b 
-
-### votre code ici
+### votre code ici ; ne rien modifier d'autre
 x = A \ b
-###
 
 # vérification
 using Test
